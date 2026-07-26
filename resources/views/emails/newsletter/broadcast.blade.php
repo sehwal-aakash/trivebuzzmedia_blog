befc793c1f5f732e9e8a237ca264fef0f058c80c
Aakash Sharma <sehwal.aakash@gmail.com>
2026-07-26 18:48:47 +0530

    feat: overhaul email templates UI with TriveBuzz logo header, card styling, and modern button design

--- resources/views/emails/newsletter/broadcast.blade.php
+++ resources/views/emails/newsletter/broadcast.blade.php
@@ -3,17 +3,17 @@
 
 {!! $content !!}
 
-<x-mail::button :url="config('app.url')">
-Read More Stories
+<x-mail::button :url="config('app.url')" color="primary">
+Explore Latest Stories on {{ config('app.name') }}
 </x-mail::button>
 
-Thanks,<br>
-{{ config('app.name') }} Team
+Warm regards,<br>
+**The {{ config('app.name', 'TriveBuzz Media') }} Editorial Team**
 
 <x-mail::subcopy>
-You are receiving this update from {{ config('app.name') }} ({{ config('app.url') }}).
+You are receiving this official update because you are subscribed to **{{ config('app.name') }}** ([{{ config('app.url') }}]({{ config('app.url') }})).
 @if(!empty($unsubscribeToken))
-If you wish to stop receiving these emails, you can [unsubscribe here]({{ route('newsletter.unsubscribe', ['token' => $unsubscribeToken]) }}).
+If you no longer wish to receive these email updates, you can [unsubscribe from newsletter]({{ route('newsletter.unsubscribe', ['token' => $unsubscribeToken]) }}) at any time.
 @endif
 </x-mail::subcopy>
 </x-mail::message>
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header" style="padding: 32px 0 20px 0; text-align: center;">
+<a href="{{ $url }}" style="display: inline-block; text-decoration: none;">
+    <img src="{{ asset('trivebuzzmedia-logo.png') }}" class="logo" alt="{{ config('app.name', 'TriveBuzz Media') }}" style="height: 44px; width: auto; max-height: 48px; object-fit: contain; vertical-align: middle;">
+</a>
+</td>
+</tr>
